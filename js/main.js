d16f94eb29bcebc7128bf5a6a7073a8ccedcd184
Jesse <jreye039@ucr.edu>
2018-12-16 13:33:31 -0800

    updated html/css

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-// Animate smooth scolling
-$('#view-projs').on('click', function(){
+// animate smooth scolling
+$('#view-work').on('click', function(){
 	const images = $('#images').position.top();
 
 	$('html, body').animate(
 		{
 			scrollTop: images
 		},
-		1000
+		900
 	);
 });
